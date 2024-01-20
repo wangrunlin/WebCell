44349994a838c91e8f7653d79131ebd9468ead33
TechQuery <TechQuery@users.noreply.github.com>
2024-01-20 00:29:05 +0000

    deploy: 4ce82d8e5be08506fac08929f63d470819fdadd5

---
+++ preview/Async.88d23f33.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire5b94;(0,e.register)("80YIH",function(r,l){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var i=e("6SIla"),a=({children:e})=>(0,i.jsxs)("div",{children:["Async load: ",e]})});
+//# sourceMappingURL=Async.88d23f33.js.map
